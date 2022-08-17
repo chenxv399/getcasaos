d120a9c13e306f60a7faa5b0b57e3b09c11498c8
chenxv399 <92201935+chenxv399@users.noreply.github.com>
2022-08-17 10:02:51 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -436,7 +436,7 @@ Download_CasaOS() {
     else
         Casa_Tag="$version"
     fi
-    Casa_Package_URL="https://gitee.com/chenxv399/CasaOS/releases/download/v0.3.4/linux-amd64-casaos.tar.gz"
+    Casa_Package_URL="https://gitee.com/chenxv399/CasaOS/releases/download/${Casa_Tag}/${Casa_Package}"
     # Remove Temp File
     ${sudo_cmd} rm -rf "$PREFIX/tmp/${Casa_Package}"
     # Download Package
